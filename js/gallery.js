202e428d89646b92df126dc132ec75367a9071d8
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-09-18 10:09:24 +0300

    added gallery menu

--- js/gallery.js
+++ js/gallery.js
@@ -2,6 +2,7 @@
 
 const url = "http://localhost:1337";
 const slideshow = document.getElementById("slideshow");
+const slideshowmenu = document.getElementById("slideshowmenu");
 
 fetch(url + "/galleries/")
   .then((res) => res.json())
@@ -12,5 +13,15 @@ fetch(url + "/galleries/")
       <img src="${url + image.url}" alt="${image.alternativeText}" uk-cover>
   </li>
       `;
+    })
+  );
+
+fetch(url + "/galleries/")
+  .then((res) => res.json())
+  .then((data) =>
+    data.forEach((title) => {
+      slideshowmenu.innerHTML += `
+    <li><a href="#">${title.Title}</a></li>
+    `;
     })
   );
