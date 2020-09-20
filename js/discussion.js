27f4b42ffaca28ef6fa250aa2843447cf7fd4968
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-09-20 12:38:57 +0300

    small fixes

--- js/discussion.js
+++ js/discussion.js
@@ -24,17 +24,18 @@ fetch(url + "/discussions")
   .then((data) => {
     data.forEach((e) => {
       console.log(e);
+      const date = e.created_at.slice(0, 10);
       comments.innerHTML += `
       <article class="uk-comment">
     <header class="uk-comment-header">
         <div class="uk-grid-medium uk-flex-middle" uk-grid>
             <div class="uk-width-auto">
-            <span uk-icon="user"></span>
+            <span uk-icon="icon: user; ratio: 2"></span>
             </div>
             <div class="uk-width-expand">
                 <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">${e.Name}</a></h4>
                 <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
-                    <li><a href="#">${e.created_at}</a></li>
+                    <li><a href="#">${date}</a></li>
                     <li><a href="#">Reply</a></li>
                 </ul>
             </div>
@@ -43,6 +44,7 @@ fetch(url + "/discussions")
     <div class="uk-comment-body">
         <p>${e.Text}</p>
     </div>
+    <hr class="uk-divider-small">
 </article>
       `;
     });
