0268f41979e3fea39a3a07135a9fc560f0e26424
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-09-20 12:03:24 +0300

    added fetch to discussion

--- js/discussion.js
+++ js/discussion.js
@@ -1,1 +1,19 @@
 /** @format */
+
+const url = "http://localhost:1337";
+
+document.forms[0].addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  fetch(url + "/discussions/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      Name: e.target.elements.name.value,
+      Email: e.target.elements.email.value,
+      Text: e.target.elements.comment.value,
+    }),
+  });
+});
