5dc791be0fe78319c2e3cc6561c5c861e2f39a88
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-09-23 16:39:04 +0300

    transfered some code to functions

--- js/discussion.js
+++ js/discussion.js
@@ -3,26 +3,67 @@
 const url = "http://localhost:1337";
 const comments = document.getElementById("comments");
 
+// Function for creating a comment
+function addComment(data) {
+  data.forEach((e) => {
+    const date = e.created_at.slice(0, 10);
+    comments.innerHTML += `
+        <article class="uk-comment">
+          <header class="uk-comment-header">
+            <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                <div class="uk-width-auto">
+                  <span uk-icon="icon: user; ratio: 2"></span>
+                </div>
+                <div class="uk-width-expand">
+                  <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">${e.Name}</a></h4>
+                  <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
+                    <li>${date}</li>
+                  </ul>
+                </div>
+            </div>
+          </header>
+            <div class="uk-comment-body">
+              <p>${e.Text}</p>
+            </div>
+            <hr class="uk-divider-small">
+        </article>
+    `;
+  });
+}
+
+function fetchData() {
+  // Fetch data for creating a comment
+  fetch(url + "/discussions")
+    .then((res) => res.json())
+    .then((data) => {
+      addComment(data);
+    })
+    .catch(() => {
+      UIkit.notification("Server error!", {
+        status: "danger",
+        pos: "bottom-center",
+      });
+    });
+}
+fetchData();
 // POST a comment to DB
 document.forms[0].addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const name = e.target.elements.name.value;
-  const email = e.target.elements.email.value;
-  const comment = e.target.elements.comment.value;
+  const body = {
+    Name: e.target.elements.name.value,
+    Email: e.target.elements.email.value,
+    Text: e.target.elements.comment.value,
+  };
 
   // Input field "Name" validation and error handling
-  if (name.match(/^[A-z ,.'-]+$/i)) {
+  if (body.Name.match(/^[A-z ,.'-]+$/i)) {
     fetch(url + "/discussions/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        Name: name,
-        Email: email,
-        Text: comment,
-      }),
+      body: JSON.stringify(body),
     })
       .then((data) => data.json())
       .then(() => {
@@ -31,6 +72,7 @@ document.forms[0].addEventListener("submit", (e) => {
           pos: "bottom-center",
         });
       })
+      .then(() => fetchData())
       .catch(() => {
         UIkit.notification("Server error!", {
           status: "danger",
@@ -44,39 +86,3 @@ document.forms[0].addEventListener("submit", (e) => {
     });
   }
 });
-
-// Create comment article
-fetch(url + "/discussions")
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((e) => {
-      const date = e.created_at.slice(0, 10);
-      comments.innerHTML += `
-      <article class="uk-comment">
-    <header class="uk-comment-header">
-        <div class="uk-grid-medium uk-flex-middle" uk-grid>
-            <div class="uk-width-auto">
-            <span uk-icon="icon: user; ratio: 2"></span>
-            </div>
-            <div class="uk-width-expand">
-                <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">${e.Name}</a></h4>
-                <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
-                    <li>${date}</li>
-                </ul>
-            </div>
-        </div>
-    </header>
-    <div class="uk-comment-body">
-        <p>${e.Text}</p>
-    </div>
-    <hr class="uk-divider-small">
-</article>
-      `;
-    });
-  })
-  .catch(() => {
-    UIkit.notification("Server error!", {
-      status: "danger",
-      pos: "bottom-center",
-    });
-  });
