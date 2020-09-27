98e9aac033e733497c1d245246bce065c0f22e54
Dovydas Zilinskas <user@Dovydas-MacBook-Air.local>
2020-09-28 00:13:31 +0300

    Added auth to write comment and rm delete button

--- js/discussion.js
+++ js/discussion.js
@@ -4,44 +4,45 @@ const url = "http://localhost:1337";
 const comments = document.getElementById("comments");
 
 // Function for deleting a comment
-function deleteComment() {
-  document.querySelectorAll("button.userid").forEach((button) => {
-    button.addEventListener("click", (e) => {
-      const article =
-        e.target.parentElement.parentElement.parentElement.parentElement
-          .parentElement.parentElement;
-      const id = e.target.value;
-      fetch(url + "/discussions/" + id, {
-        method: "DELETE",
-      })
-        .then((data) => data.json())
-        .then(() => {
-          UIkit.notification("Successfuly deleted", {
-            status: "success",
-            pos: "bottom-center",
-          });
-        })
-        .then(() => {
-          comments.removeChild(article);
-        })
-        .catch(() => {
-          if (id == "") {
-            UIkit.notification("Please login to delete post", {
-              status: "warning",
-              pos: "bottom-center",
-            });
-          } else {
-            UIkit.notification("Server error!", {
-              status: "danger",
-              pos: "bottom-center",
-            });
-          }
-        });
-    });
-  });
-}
+// function deleteComment() {
+//   document.querySelectorAll("button.userid").forEach((button) => {
+//     button.addEventListener("click", (e) => {
+//       const article =
+//         e.target.parentElement.parentElement.parentElement.parentElement
+//           .parentElement.parentElement;
+//       const id = e.target.value;
+//       fetch(url + "/discussions/" + id, {
+//         method: "DELETE",
+//       })
+//         .then((data) => data.json())
+//         .then(() => {
+//           UIkit.notification("Successfuly deleted", {
+//             status: "success",
+//             pos: "bottom-center",
+//           });
+//         })
+//         .then(() => {
+//           comments.removeChild(article);
+//         })
+//         .catch(() => {
+//           if (id == "") {
+//             UIkit.notification("Please login to delete post", {
+//               status: "warning",
+//               pos: "bottom-center",
+//             });
+//           } else {
+//             UIkit.notification("Server error!", {
+//               status: "danger",
+//               pos: "bottom-center",
+//             });
+//           }
+//         });
+//     });
+//   });
+// }
 
 // Function for creating a comment
+
 function addComment(data) {
   data.forEach((i) => {
     const date = i.created_at.slice(0, 10);
@@ -56,7 +57,6 @@ function addComment(data) {
                   <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">${i.Name}</a></h4>
                   <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
                     <li>${date}</li>
-                    <li><button class="userid uk-button uk-button-default uk-button-small" value="${i.id}">Delete</button></li>
                   </ul>
                 </div>
             </div>
@@ -68,21 +68,21 @@ function addComment(data) {
         </article>
     `;
   });
-  deleteComment();
+  // deleteComment();
 }
 
 // Firebase authentication state viewer to display or hide page elements
 function displayNone() {
   const el = document.getElementById("lilogin");
-  const all = document.getElementsByClassName("userid");
+  // const all = document.getElementsByClassName("userid");
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       el.innerHTML = `<a href="#" id="fbsignout" onclick="signoutUser()">Signout</a>`;
     } else {
       el.innerHTML = `<a href="#">Login</a>`;
-      for (let i = 0; i < all.length; i++) {
-        all[i].removeAttribute("value");
-      }
+      // for (let i = 0; i < all.length; i++) {
+      //   all[i].removeAttribute("value");
+      // }
     }
   });
 }
@@ -122,46 +122,58 @@ fetch(url + "/discussions")
     });
   });
 
-// POST a comment to DB on submit
-document.forms[0].addEventListener("submit", (e) => {
-  e.preventDefault();
+// POST a comment to DB on submit while checking user login status
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    document.forms[0].addEventListener("submit", (e) => {
+      e.preventDefault();
 
-  const body = {
-    Name: e.target.name.value,
-    Email: e.target.email.value,
-    Text: e.target.comment.value,
-  };
+      const body = {
+        Name: e.target.name.value,
+        Email: e.target.email.value,
+        Text: e.target.comment.value,
+      };
 
-  // Input field "Name" validation and error handling
-  if (body.Name.match(/^[A-z ,.'-]+$/i)) {
-    fetch(url + "/discussions/", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
-      .then((data) => data.json())
-      .then((res) => {
-        addComment([res]);
-      })
-      .then(() => {
-        UIkit.notification("Successfuly added", {
-          status: "success",
-          pos: "bottom-center",
-        });
-      })
-      .then(() => document.forms[0].reset())
-      .catch(() => {
-        UIkit.notification("Server error!", {
+      // Input field "Name" validation and error handling
+      if (body.Name.match(/^[A-z ,.'-]+$/i)) {
+        fetch(url + "/discussions/", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        })
+          .then((data) => data.json())
+          .then((res) => {
+            addComment([res]);
+          })
+          .then(() => {
+            UIkit.notification("Successfuly added", {
+              status: "success",
+              pos: "bottom-center",
+            });
+          })
+          .then(() => document.forms[0].reset())
+          .catch(() => {
+            UIkit.notification("Server error!", {
+              status: "danger",
+              pos: "bottom-center",
+            });
+          });
+      } else {
+        UIkit.notification("Incorrect name", {
           status: "danger",
           pos: "bottom-center",
         });
-      });
+      }
+    });
   } else {
-    UIkit.notification("Incorrect name", {
-      status: "danger",
-      pos: "bottom-center",
+    document.forms[0].addEventListener("submit", (e) => {
+      e.preventDefault();
+      UIkit.notification("Your are not signed in!", {
+        status: "danger",
+        pos: "bottom-center",
+      });
     });
   }
 });
